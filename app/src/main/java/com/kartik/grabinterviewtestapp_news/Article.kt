9130f791d71943e78f167db70baa236ad12dace6
Kartik Shankhavaram <shankhavaramk@gmail.com>
2020-07-01 17:52:32 +0530

    Added room entity class for Article data object

---
+++ app/src/main/java/com/kartik/grabinterviewtestapp_news/Article.kt
@@ -0,0 +1,19 @@
+package com.kartik.grabinterviewtestapp_news
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "articles")
+data class Article(
+    @PrimaryKey(autoGenerate = true) var id: Int,
+    @ColumnInfo(name = "source_id") var sourceId: String?,
+    @ColumnInfo(name = "source_name") var sourceName: String?,
+    @ColumnInfo(name = "author") var author: String?,
+    @ColumnInfo(name = "title") var title: String?,
+    @ColumnInfo(name = "description") var desc: String?,
+    @ColumnInfo(name = "url") var url: String?,
+    @ColumnInfo(name = "url_to_image") var urlToImage: String?,
+    @ColumnInfo(name = "published_at") var publishedAt: String?,
+    @ColumnInfo(name = "content") var content: String?
+)
